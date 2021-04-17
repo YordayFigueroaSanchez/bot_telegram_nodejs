d83ae5fc76aad4aaac7ee7ca7e8e836842d6d578
Yorday Figueroa <yfsanchez@gmail.com>
2021-04-17 16:24:47 -0300

    agregando nodemon
    agregando nodemon

--- bot.js
+++ bot.js
@@ -2,8 +2,26 @@ const {Telegraf} = require('telegraf')
 
 const bot = new Telegraf('1784474668:AAG6DffL1G2VBkzzMH1X74ZEBQGsii9i8-Y')
 
-bot.start(ctx => {
+bot.start((ctx) => {
     ctx.reply('Welcome');
 })
+
+bot.help((ctx) => {
+    ctx.reply('Help !!.');
+})
+
+bot.settings((ctx) => {
+    ctx.reply('Settings .');
+})
+
+bot.command(['comando','Comando','command','Command'],(ctx) => {
+    ctx.reply('My command in use.');
+})
+
+bot.command(['aaa','bbb','ccc','ddd'],(ctx) => {
+    ctx.reply('ABCD comando.');
+})
+
+
 
 bot.launch()
